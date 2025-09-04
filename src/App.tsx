fb3a210fd66962ea7006737362fdf1f418406649
Lucas Devantier Pinto <156879610+Devantier2002@users.noreply.github.com>
2025-09-04 19:58:05 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,48 @@ import { CardVeiculo } from "./components/CardVeiculo";
 import { InputPesquisa } from "./components/InputPesquisa";
 import type { CarroType } from "./utils/CarroType";
 import { useEffect, useState } from "react";
-import { useClienteStore } from "./context/ClienteContext";
+import { useClienteStore } from "./context/ClienteContext"
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL
 
 export default function App() {
-  const [carros, setCarros] = useState<CarroType[]>([]);
-  const { logaCliente } = useClienteStore();
+  const [carros, setCarros] = useState<CarroType[]>([])
+  const { logaCliente } = useClienteStore()  
 
   useEffect(() => {
     async function buscaDados() {
-      const response = await fetch(`${apiUrl}/carros`);
-      const dados = await response.json();
-      // Verifique se a resposta é realmente um array
-      if (Array.isArray(dados)) {
-        setCarros(dados);
-      }
+      const response = await fetch(`${apiUrl}/carros`)
+      const dados = await response.json()
+//      console.log(dados)
+      setCarros(dados)
     }
+    buscaDados()
 
     async function buscaCliente(id: string) {
-      const response = await fetch(`${apiUrl}/clientes/${id}`);
-      const dados = await response.json();
-      logaCliente(dados);
+      const response = await fetch(`${apiUrl}/clientes/${id}`)
+      const dados = await response.json()
+      logaCliente(dados)
     }
-
     if (localStorage.getItem("clienteKey")) {
-      const idCliente = localStorage.getItem("clienteKey");
-      buscaCliente(idCliente as string);
-    }
+      const idCliente = localStorage.getItem("clienteKey")
+      buscaCliente(idCliente as string)
+    }    
+  }, [])
 
-    buscaDados();
-  }, []);
-
-  // Verifique se carros é um array antes de mapear
-  const listaCarros = Array.isArray(carros)
-    ? carros.map((carro) => <CardVeiculo data={carro} key={carro.id} />)
-    : [];
+  const listaCarros = carros.map( carro => (
+    <CardVeiculo data={carro} key={carro.id} />
+  ))
 
   return (
     <>
       <InputPesquisa setCarros={setCarros} />
       <div className="max-w-7xl mx-auto">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Veículos{" "}
-          <span className="underline underline-offset-3 decoration-8 decoration-orange-400 dark:decoration-orange-600">
-            em destaque
-          </span>
+          Veículos <span className="underline underline-offset-3 decoration-8 decoration-orange-400 dark:decoration-orange-600">em destaque</span>
         </h1>
-        <div className="flex gap-3">{listaCarros}</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+          {listaCarros}
+        </div>
       </div>
     </>
   );
